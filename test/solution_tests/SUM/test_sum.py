44d7c7f3eab9cd5ee7014a2ae2915ac97a35e753
sbx_user1051 <sbx_user1051@169.254.187.93>
2021-09-21 08:44:44 +0000

    Tue Sep 21 08:44:44 UTC 2021

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,8 +6,6 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
 
     inputs = [(0,0,0),(100,100,200),(0,100,100),(100,0,100),(33,47,80)]
-    @pytest.mark.parametrize("first,second,solution", inputs)
+    @pytest.mark.parametrize('first, second, solution', inputs)
     def test_sums(first, second, solution):
         assert sum_solution.compute(first, second) == solution
-
-
