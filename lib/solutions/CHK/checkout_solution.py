7f2471d86025631e2eb95fc1eb79eebaa0bf8816
sbx_user1051 <sbx_user1051@169.254.187.93>
2021-09-21 11:30:48 +0000

    Tue Sep 21 11:30:48 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,13 +2,11 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-productA = Item('A',50)
-productB = Item('B',30)
-productC = Item('C',20)
-productD = Item('D',15)
-productE = Item('E',40)
-products = { 'A': productA, 'B': productB, 'C': productC, 'D': productD, 'E': productE}
-offers = [Offer({productA: 3}, 130), Offer({productA: 5}, 200), Offer({ProductB: 2}, 45), Offer({ProductB: 1, ProductE: 2}, 80)]
+
+class InvalidOperationException(Exception):
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
 
 def checkout(skus):
     basket = Basket()
@@ -19,6 +17,20 @@ def checkout(skus):
             return -1
     return basket.calculate_value
 
+class Offer:
+    def __init__(self, combinationDict, dealPrice):
+        self.combinationDict = combinationDict
+        self.dealPrice = dealPrice
+
+    def apply_offer(basket):
+        for key,value in offer.combindationDict.Items():
+            basket.remove_items(key, value)
+        basket.add_item_price(offer.dealPrice)
+
+class Item:
+    def __init__(self, name, standardPrice):
+        self.name = name
+        self.standardPrice = standardPrice
 
 class Basket:
     def __init__(self):
@@ -48,7 +60,7 @@ class Basket:
     def calculate_value():
         new_potential_baskets = apply_offers()
         if new_potential_baskets:
-            return min(new_potential_baskets, x => x.calculate_value)
+            return min(new_potential_baskets, lambda x: x.calculate_value)
         for item, count in self.items.Items():
             self.price += item.standardPrice * count
         return self.price
@@ -69,25 +81,13 @@ class Basket:
             basket.viableOffers = self.viable_offers
         return new_potential_baskets
 
-    
-        
-class Offer:
-    def __init__(self, combination, dealPrice):
-        self.combinationDict = combinationDict
-        self.dealPrice = dealPrice
 
-    def apply_offer(basket):
-        for key,value in offer.combindationDict.Items():
-            basket.remove_items(key, value)
-        basket.add_item_price(offer.dealPrice)
+productA = Item('A',50)
+productB = Item('B',30)
+productC = Item('C',20)
+productD = Item('D',15)
+productE = Item('E',40)
+products = { 'A': productA, 'B': productB, 'C': productC, 'D': productD, 'E': productE}
+offers = [Offer({productA: 3}, 130), Offer({productA: 5}, 200), Offer({ProductB: 2}, 45), Offer({ProductB: 1, ProductE: 2}, 80)]
 
-class Item:
-    def __init__(self, name, standardPrice):
-        self.name = name
-        self.standardPrice = standardPrice
-
-class InvalidOperationException(Excetion):
-    def __init__(self, expression, message):
-        self.expression = expression
-        self.message = message
 
