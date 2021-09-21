610f524e1e1145f4de85c34efd57753d337e1b32
sbx_user1051 <sbx_user1051@169.254.187.93>
2021-09-21 11:27:49 +0000

    Tue Sep 21 11:27:49 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,7 @@ productB = Item('B',30)
 productC = Item('C',20)
 productD = Item('D',15)
 productE = Item('E',40)
-products = { 'A': productA, 'B': productB, 'C': productC, 'D': productD, productE]
+products = { 'A': productA, 'B': productB, 'C': productC, 'D': productD, 'E': productE}
 offers = [Offer({productA: 3}, 130), Offer({productA: 5}, 200), Offer({ProductB: 2}, 45), Offer({ProductB: 1, ProductE: 2}, 80)]
 
 def checkout(skus):
@@ -22,8 +22,14 @@ def checkout(skus):
 
 class Basket:
     def __init__(self):
-        self.items={}
+        self.viable_offers = offers
+        self.items= {}
         self.price = 0
+
+    def __init__(self, viable_offers, items, price):
+        self.viable_offers = viable_offers
+        self.items = items
+        self.price = price
 
     def add_item(item):
         if item in self.items:
@@ -33,36 +39,55 @@ class Basket:
 
     def remove_items(item, count):
         if self.items[item] < count:
-            throw
+            raise InvalidOperationException("Too few items to remove")
+        self.items[item] = self.items[item] - count
+
+    def add_item_price(price):
+        self.price = self.price + price
     
     def calculate_value():
-        
-    def apply_offer(offer):
-        
-        for key,value in combindationDict:
-            basket[key] = basket[key] - value
-        basket.price += self.dealPrice
+        new_potential_baskets = apply_offers()
+        if new_potential_baskets:
+            return min(new_potential_baskets, x => x.calculate_value)
+        for item, count in self.items.Items():
+            self.price += item.standardPrice * count
+        return self.price
 
+    def apply_offers():
+        unviable_offers = []
+        new_potential_baskets = [] 
+        for offer in self.viable_offers:
+            try:
+                basket = Basket(self.viable_offers, self.items, self.price)
+                offer.apply_offer(basket)
+                new_potential_baskets.add(basket)
+            except:
+                unviable_offers = offer
+        for unviable_offer in unviable_offers:
+            self.viable_offers.remove(unviable_offer)
+        for basket in new_potential_baskets:
+            basket.viableOffers = self.viable_offers
+        return new_potential_baskets
+
+    
+        
 class Offer:
     def __init__(self, combination, dealPrice):
         self.combinationDict = combinationDict
         self.dealPrice = dealPrice
 
-    def offer_applicable():
-        for key,value in self.combinationDict.items():
-            try:
-                if self.items[key] < value:
-                    return false
-            except:
-                return false
-        return true
+    def apply_offer(basket):
+        for key,value in offer.combindationDict.Items():
+            basket.remove_items(key, value)
+        basket.add_item_price(offer.dealPrice)
 
 class Item:
     def __init__(self, name, standardPrice):
         self.name = name
         self.standardPrice = standardPrice
 
-class InvalidOfferException(Excetion):
+class InvalidOperationException(Excetion):
     def __init__(self, expression, message):
         self.expression = expression
         self.message = message
+
