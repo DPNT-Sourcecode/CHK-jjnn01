4fd433422cd9ff952ffb3d4709ae7e9f07e21258
sbx_user1051 <sbx_user1051@169.254.187.93>
2021-09-21 09:01:27 +0000

    Tue Sep 21 09:01:27 UTC 2021

---
+++ test/solution_tests/HLO/test_sum.py
@@ -0,0 +1,14 @@
+from lib.solutions.SUM import sum_solution
+
+class TestSum():
+    def test_sum(self):
+        assert sum_solution.compute(1, 2) == 3
+
+    def test_min(self):
+        assert sum_solution.compute(0, 0) == 0
+
+    def test_max(self):
+        assert sum_solution.compute(100, 100) == 200
+
+    def test_random(self):
+        assert sum_solution.compute(40, 33) == 73
