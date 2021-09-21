6d96ae0f5d165259dd34d760f29cc041c37285e9
sbx_user1051 <sbx_user1051@169.254.187.93>
2021-09-21 09:25:03 +0000

    Tue Sep 21 09:25:03 UTC 2021

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -2,4 +2,35 @@ from lib.solutions.CHK import checkout_solution
 
 class TestCHK():
     def test_singleA(self):
-        assert checkout_solution.hello('John') == 'Hello, John!'
+        assert checkout_solution.checkout('A') == 50
+
+
+    def test_singleB(self):
+        assert checkout_solution.checkout('B') == 30
+
+
+    def test_singleC(self):
+        assert checkout_solution.checkout('C') == 20
+
+
+    def test_singleC(self):
+        assert checkout_solution.checkout('D') == 15
+
+    def test_twoA(self):
+        assert checkout_solution.checkout('AA') == 100
+
+    def test_threeA(self):
+        assert checkout_solution.checkout('AAA') == 130
+
+    def test_fourA(self):
+        assert checkout_solution.checkout('AAAA') == 180
+
+    def test_twoB(self):
+        assert checkout_solution.checkout('BB') == 45
+
+    def test_multiplediscountswhennested(self):
+        assert checkout_solution.checkout('ABABA') == 175
+
+
+
+
